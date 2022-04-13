ca6ea4ceb09ed2a9a8fa8c01948ff5cffd18457a
Harsimran Bhinder <bhinderrr@gmail.com>
2022-04-13 20:09:54 +0000

    snake updates

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net')
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  const name = "LuL";
+
+  conn.setEncoding("utf8");
+  conn.on('data', (data) => {
+    console.log('Server sent you a msg!');
+    console.log(data);
+  })
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+    conn.write(`Name: ${name}`);
+  })
+
+  return conn;
+};
+
+module.exports = connect
