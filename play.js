724375a8de06e7001592ff6ca958847f79916228
Harsimran Bhinder <bhinderrr@gmail.com>
2022-04-13 21:18:15 +0000

    input modules

--- play.js
+++ play.js
@@ -1,28 +1,11 @@
-const { write } = require('fs');
 const net = require('net');
 
-const connect = require("./client");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input")
 
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-  // if (key === "w") {
-  //   console.log("Move: up");
-  // }
-};
-
-setupInput()
 
 console.log('Connecting ...');
 connect();
+
+setupInput();
